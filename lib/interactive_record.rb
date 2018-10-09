4c6321c0736ffae0b861a03b8f2ab06564bd309d
Achsah Abraham <achsahmp@gmail.com>
2018-10-09 16:17:07 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -19,10 +19,11 @@ class InteractiveRecord
   end
 
   def initialize(new_student={})
-      options.each do |name, grade|
-        self.send("#{name}=", grade)
-      end
+    options.each do |name, grade|
+      self.send("#{name}=", grade)
     end
+  end
+
   def table_name_for_insert(table_name)
     self.class.table_name
   end
