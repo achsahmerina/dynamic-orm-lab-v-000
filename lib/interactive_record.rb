b9333d6d898f4e7668d81d67c8d1990344b0d342
Achsah Abraham <achsahmp@gmail.com>
2018-10-09 16:27:50 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -61,7 +61,10 @@ class InteractiveRecord
     row = attribute.keys[0].to_s
     value = attribute.values[0]
 
-    sql = <<-SQL SELECT * FROM #{table_name} WHERE #{row} = ?
-      SQL
+    sql = <<-SQL
+      SELECT * FROM #{table_name} WHERE #{row} = ?
+    SQL
+
+    DB[:conn].execute(sql, value)
   end
 end
